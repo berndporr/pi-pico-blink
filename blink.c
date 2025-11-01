2ef08eb94a52771ecce3ddf63b9db80cd20a0b23
Bernd Porr <mail@berndporr.me.uk>
2025-11-01 17:54:53 +0000

    Done it properly with a repeating timer and main() just sleeps.

--- blink.c
+++ blink.c
@@ -24,12 +24,21 @@ void pico_set_led(bool led_on) {
     gpio_put(n, led_on);
 }
 
+// global state of the LED but should be done via the
+// user data of the repeating timer.
+bool led_state = false;
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+    led_state = !led_state;
+    pico_set_led(led_state);
+    return true;
+}
+
 int main() {
-    bool b = true;
     pico_led_init();
+    struct repeating_timer timer;
+    add_repeating_timer_ms(-500, repeating_timer_callback, NULL, &timer);
     while (true) {
-        pico_set_led(b);
-	b = !b;
-        sleep_ms(LED_delay_ms);
+	sleep_ms(0xffffffff);
     }
 }
